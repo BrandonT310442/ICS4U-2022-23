6cc5e8a4e64ca1e4dc0e11787862f847b0d2db5f
Brandon Tai <btai@bayviewglen.ca>
2023-01-31 09:50:15 -0500

    Vue Intro

---
+++ Vue/vue3-intro/creating-vue-app/main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+
+    data(){
+        return {
+            product : 'Boots',
+            description:"Sock Co. is a leading retailer of stylish and comfortable socks for all occasions. We offer a wide range of designs and materials, from classic cotton to performance blends, in a variety of sizes and styles to fit everyone's needs. Our commitment to quality and customer satisfaction has made us a trusted name in the sock industry. Shop with us for the ultimate sock experience!"
+        }
+    }
+});
